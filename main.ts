2b01396a150d9578a70a151bd015d4391925890e
jaimecreer <70456956+jaimecreer@users.noreply.github.com>
2020-08-31 00:21:57 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,48 @@
-
+function drawTriangle () {
+    for (let row = 0; row <= 4; row++) {
+        for (let column = 0; column <= row; column++) {
+            led.plot(column, row)
+        }
+    }
+}
+function drawDiagonal2 () {
+    for (let row = 0; row <= 4; row++) {
+        led.plot(4 - row, row)
+    }
+}
+function drawRow (row: number) {
+    for (let column = 0; column <= 4; column++) {
+        led.plot(column, row)
+    }
+}
+function drawChecker () {
+    for (let row = 0; row <= 4; row++) {
+        for (let column = 0; column <= 4; column++) {
+            if ((row + column) % 2 == 0) {
+                led.plot(column, row)
+            }
+        }
+    }
+}
+function drawTriangleV2 () {
+    for (let row = 0; row <= 4; row++) {
+        for (let column = 0; column <= 4; column++) {
+            if (row + column <= 4) {
+                led.plot(column, row)
+            }
+        }
+    }
+}
+function drawSq () {
+    for (let row = 0; row <= 4; row++) {
+        for (let column = 0; column <= 4; column++) {
+            led.plot(column, row)
+        }
+    }
+}
+function drawDiagonal () {
+    for (let row = 0; row <= 4; row++) {
+        led.plot(row, row)
+    }
+}
+drawChecker()
